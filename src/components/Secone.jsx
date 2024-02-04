a1418b47200aea649ad635f70811e9f383f586cc
patrickashi <patrickpeko1@gmail.com>
2024-02-04 08:44:57 +0100

    new
    add new

--- src/components/Secone.jsx
+++ src/components/Secone.jsx
@@ -6,7 +6,7 @@ const Secone = () => {
     <div className='w-full py-[30px] font-nunito'>
         <div className='mobile:grid 2xl:grid-cols-2 mx-[40px] mobile:mx-0'>
             <div className='2xl:mx-[40px] mobile:ml-4 2xl:py-8'>
-                <p className='2xl:text-[50px] 2xl:py-4 mobile:text-xl'>Up Your <span className='text-[#0504AA]'> Skills </span> 
+                <p className='2xl:text-[50px] 2xl:py-4 mobile:text-[25px] '>Up Your <span className='text-[#0504AA]'> Skills </span> 
                 to <span className='text-[#0504AA]'>advance</span> your 
                 </p>
                 <p className='2xl:text-[50px] 2xl:py-4 mobile:text-xl' ><span className='text-[#0504AA]'>career</span>  path..</p>
