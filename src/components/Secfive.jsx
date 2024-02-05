d39a7fb57623b98dc786365de1d737795e22314a
patrickashi <patrickpeko1@gmail.com>
2024-02-05 22:53:53 +0100

    new
    add new

--- src/components/Secfive.jsx
+++ src/components/Secfive.jsx
@@ -12,21 +12,21 @@ const Secfive = () => {
         <div className='flex justify-center flex-col items-center py-6'>
             <h1 className='text-3xl mb-2'>Our most Popular <span className='text-[#0504AA]'>Courses</span></h1>
             <div className='flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col mobile:flex-col gap-5'>
-                <div className='px-4 py-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
+                <div className='px-4 mobile:mx-4 py-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
                     <div className=' w-[380px] '>
                         <img src={car1} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
                     <p className='py-2'>Fundamentals of Electrical Engineering Facility Design</p>
                 </div>
 
-               <div className='px-4 py-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
+               <div className='px-4 py-4 mobile:mx-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
                     <div className=' w-[380px] '>
                         <img src={car4} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
                     <p className='py-2'>Fundamentals of Electrical Engineering Facility Design</p>
                 </div>
 
-                <div className='px-4 py-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
+                <div className='px-4 py-4 mobile:mx-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
                     <div className=' w-[380px] '>
                         <img src={car5} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
