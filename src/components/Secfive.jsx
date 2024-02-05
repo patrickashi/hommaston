6320d2b511971ce1e5cc4422cdd87585e7c2360d
patrickashi <patrickpeko1@gmail.com>
2024-02-05 23:18:10 +0100

    new
    add new

--- src/components/Secfive.jsx
+++ src/components/Secfive.jsx
@@ -13,24 +13,24 @@ const Secfive = () => {
             <h1 className='text-3xl mb-2'>Our most Popular <span className='text-[#0504AA]'>Courses</span></h1>
             <div className='flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col mobile:flex-col gap-5'>
                 <div className='px-4 mobile:mx-4 py-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
-                    <div className=' w-[380px] '>
+                    <div className=' w-[340px] '>
                         <img src={car1} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
                     <p className='py-2'>Fundamentals of Electrical Engineering Facility Design</p>
                 </div>
 
                <div className='px-4 py-4 mobile:mx-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
-                    <div className=' w-[380px] '>
+                    <div className=' 2xl:w-[380px] mobile:w-[330px] '>
                         <img src={car4} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
-                    <p className='py-2'>Fundamentals of Electrical Engineering Facility Design</p>
+                    <p className='py-2 sm:mx-[68px] mobile:mx-[6px] 2xl:mx-0 xl:mx-0 lg:mx-0'>Fundamentals of Instrumentation and control Engineering Facility Design</p>
                 </div>
 
                 <div className='px-4 py-4 mobile:mx-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
-                    <div className=' w-[380px] '>
+                    <div className=' 2xl:w-[380px] xl:w-[380px] lg:w-[380px] md:w-[380px] sm:w-[380px] mobile:w-[330px] '>
                         <img src={car5} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
-                    <p className='py-2'>Fundamentals of Electrical Engineering Facility Design</p>
+                    <p className='py-2 sm:mx-[68px] mobile:mx-[6px] 2xl:mx-0 xl:mx-0 lg:mx-0'>Fundamentals of Mechanical/Piping Engineering Facility Design</p>
                 </div>
             </div>
 
