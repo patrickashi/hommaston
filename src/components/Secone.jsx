5f978b160204467a0fe8e415961adfa77b85ac30
patrickashi <patrickpeko1@gmail.com>
2024-02-06 13:49:13 +0100

    new
    add new

--- src/components/Secone.jsx
+++ src/components/Secone.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import sponge from "../assets/sponge.gif"
+import sponge from "../assets/sponge.gif";
+import hommastonlogo from "../assets/hommastonlogo.png";
+import { PiDropboxLogoDuotone } from "react-icons/pi";
 
 const Secone = () => {
   return (
-    <div className='w-full py-[30px] font-nunito border-b border-gray-200 font-Montserrat'>
+    <div className='w-full py-[30px] border-b border-gray-200 font-Montserrat'>
         <div className='mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 mx-[40px] mobile:mx-0'>
-            <div className='2xl:mx-[40px] mobile:ml-4 2xl:py-8'>
+            <div className='2xl:mx-[40px] mobile:ml-4 2xl:py-4 xl:py-4 lg:py-4 md:py-4 sm:py-4'>
 
                 <p className='2xl:text-[50px] 2xl:py-4 mobile:my-8 2xl:my-0 xl:my-0 lg:my-0 md:my-8 mobile:text-[40px] font-bold '>Up Your <span className='text-[#0504AA]'> Skills </span> 
                 to <span className='text-[#0504AA]'>advance</span> your <span className='text-[#0504AA]'>career </span>path
@@ -27,6 +29,25 @@ const Secone = () => {
               <img src={sponge} alt="img" className='w-80' />
             </div>
         </div>
+
+        {/* logos */}
+        <div className='flex 2xl:flex-row xl:flex-row lg:flex-row mobile:flex-col 2xl:mx-20 xl:mx-20 lg:mx-20 mobile:mx-4 py-14 px-6 border-2 rounded-lg gap-8'>
+          <div>
+            <p className='font-bold text-2xl'>Worldwide network of Trusted Partners and clients</p>
+          </div>
+          <div className='flex justify-between 2xl:gap-20 xl:gap-20 lg:gap-8 mobile:gap-8 flex-wrap'>
+
+            <div><PiDropboxLogoDuotone size={40} alt="logo"/></div>
+            <div><PiDropboxLogoDuotone size={40} alt="logo"/></div>
+            <div><PiDropboxLogoDuotone size={40} alt="logo"/></div>
+            <div><PiDropboxLogoDuotone size={40} alt="logo"/></div>
+            <div><PiDropboxLogoDuotone size={40} alt="logo"/></div>
+            <div><PiDropboxLogoDuotone size={40} alt="logo"/></div>
+           
+          </div>
+        </div>
+
+
     </div>
   )
 }
