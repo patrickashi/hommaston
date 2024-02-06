1fd35c2c35b04e782ac06b8af6e3568c8139f9ab
patrickashi <patrickpeko1@gmail.com>
2024-02-06 23:09:37 +0100

    new
    add new

--- src/components/Secfive.jsx
+++ src/components/Secfive.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import blacklivesmatter from "../assets/blacklivesmatter.jpg";
-import blue1 from "../assets/blue1.jpg";
-import cabinatur from "../assets/cabinatur.jpg";
-import car1 from "../assets/car1.jpg";
-import car4 from "../assets/car4.jpg";
-import car5 from "../assets/car5.jpg";
+
+
+import electricalfacilitydesign from "../assets/electricalfacilitydesign.jpg";
+import instrumentationandcontrol from "../assets/instrumentationandcontrol.jpg";
+import mechanicalpiping from "../assets/mechanicalpiping.jpg";
+
 
 const Secfive = () => {
   return (
@@ -14,23 +14,23 @@ const Secfive = () => {
             <div className='flex py-16 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col mobile:flex-col mobile:gap-[100px] sm:gap-[100px] md:gap-0 lg:gap-0 xl:gap-0 2xl:gap-5'>
                 <div className='px-4 mobile:mx-4 py-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
                     <div className=' w-[340px] '>
-                        <img src={car1} className='h-25 w-cover justify-center rounded-md' alt="img" />
+                        <img src={electricalfacilitydesign} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
-                    <p className='py-2'>Fundamentals of Electrical Engineering Facility Design</p>
+                    <p className='py-2 text-center'>Fundamentals of Electrical Engineering Facility Design</p>
                 </div>
 
                <div className='px-4 py-4 mobile:mx-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
                     <div className=' 2xl:w-[380px] mobile:w-[330px] '>
-                        <img src={car4} className='h-25 w-cover justify-center rounded-md' alt="img" />
+                        <img src={instrumentationandcontrol} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
-                    <p className='py-2 2xl:w-[400px] mobile:w-[380px] text-center sm:mx-[68px] mobile:mx-[6px] 2xl:mx-0 xl:mx-0 lg:mx-0'>Fundamentals of Instrumentation and control Engineering Facility Design</p>
+                    <p className='py-2 text-center sm:mx-[68px] mobile:mx-[6px] 2xl:mx-0 xl:mx-0 lg:mx-0'>Fundamentals of Instrumentation and control Engineering Facility Design</p>
                 </div>
 
                 <div className='px-4 py-4 mobile:mx-4 flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
                     <div className=' 2xl:w-[380px] xl:w-[380px] lg:w-[380px] md:w-[380px] sm:w-[380px] mobile:w-[330px] '>
-                        <img src={car5} className='h-25 w-cover justify-center rounded-md' alt="img" />
+                        <img src={mechanicalpiping} className='h-25 w-cover justify-center rounded-md' alt="img" />
                     </div>
-                    <p className='py-2 sm:mx-[68px] mobile:mx-[6px] 2xl:mx-0 xl:mx-0 lg:mx-0'>Fundamentals of Mechanical/Piping Engineering Facility Design</p>
+                    <p className='py-2 sm:mx-[68px] mobile:mx-[6px] text-center 2xl:mx-0 xl:mx-0 lg:mx-0'>Fundamentals of Mechanical/Piping Engineering Facility Design</p>
                 </div>
             </div>
 
