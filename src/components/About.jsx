380b0168a43d542bc5b7877edfd598605b97e793
patrickashi <patrickpeko1@gmail.com>
2024-02-08 09:29:43 +0100

    new
    add new

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <div >
         <div className=' w-full h-[300px] font-Montserrat'>
             <img src={webdesign2}  alt="img" size={20} className='h-full w-full object-cover z-0' />
-            <div className='absolute mt-[-55px] mx-auto border-2 flex justify-center items-center inset-x-0 bg-gray-100/10
+            <div className='absolute mt-[-55px] mx-auto border-2 flex justify-center items-center inset-x-0 bg-gray-700/20
             2xl:mx-20 mobile:mx-5 rounded-2xl shadow-md shadow-violet-400 mx-auto z-0'>
                 <h1 className='2xl:text-6xl mobile:text-3xl py-4 font-bold'>About Us</h1>
             </div>
