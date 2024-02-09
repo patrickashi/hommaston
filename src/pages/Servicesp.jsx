28efdab7d79cb8552286ee6a6c153b7702e25768
patrickashi <patrickpeko1@gmail.com>
2024-02-09 22:09:00 +0100

    new
    add new

--- src/pages/Servicesp.jsx
+++ src/pages/Servicesp.jsx
@@ -7,7 +7,80 @@ const Servicesp = () => {
     <div className=' h-screen font-Montserrat'>
         <Servicessecone />
 
-        <div>
+        <div className='2xl:mx-20 xl:mx-20 lg:mx-20 md:mx-10 sm:mx-8 mobile:mx-4 py-10'>
+
+          <div>
+            <p>
+              HOMMASTON provides structured On-job Training consultancy aimed at enhancing organizational productivity 
+              and profitability. As a value-added proposition, we further aid client organizations in identifying critical
+              competency gaps with respect to personnel performance outcomes, evaluate the extent and propose a structured
+              plan to appropriately address them. We are an organization with a mission to transform non-experienced workforce
+              entrants into invaluable workforce assets for industry. In our approach, we associate our baseline solution to
+              include Fresh graduates facing the challenge of non-employment due to the ‘no-experience’ syndrome or the dilemma
+              of a new entrant in result-oriented project teams. We bridge this gap through intensive preparatory courses
+              and live case study structured dummy projects. The key objectives are to optimize trainees’ employment 
+              chances by equipping them with appropriate proficiency to competently deliver on live projects with little
+              or average supervision
+            </p>
+          </div>
+
+          <div className='py-8'>
+            <h1 className='font-bold text-2xl 2xl:text-center xl:text-center lg:text-center md:text-center sm:text-left mobile:text-left mb-4'>Why Choose Us?</h1>
+
+            <p>
+              <span className='font-bold'>Unparalleled Expertise:</span> With years of experience in the industry, our team of seasoned professionals 
+              possesses the knowledge and skill to deliver comprehensive training programs tailored to your specific needs.
+            </p>
+
+            <p className='my-4'>
+              <span className='font-bold'>Cutting-edge Curriculum:</span> Our training modules are thoughtfully designed, incorporating the latest industry trends
+              and best practices. We equip our trainees with the knowledge and hands-on experience required to excel in their respective fields.
+            </p>
+            <p className='my-4'>
+              <span className='font-bold'>Practical Training Approach:</span> Hommaston Limited believes in learning by doing. Our practical approach to training allows participants
+              to grasp concepts efficiently and implement them effectively.
+            </p>
+            <p className='my-4'> 
+              <span className='font-bold'>Top-notch Facilities:</span> To ensure an optimal learning environment, we boast state-of-the-art facilities equipped with modern technology
+              and resources to support our trainees' growth.
+            </p>
+            <p className='my-4'>
+              <span className='font-bold'>Dedicated Faculty:</span> Our instructors are not only subject matter experts but also passionate mentors committed to guiding our trainees
+              throughout their learning journey.
+            </p>
+            <p >
+              <span className='font-bold'>Industry Partnerships:</span> We have established strong partnerships with leading companies in various industries, providing our
+              trainees with access to potential employment opportunities.
+            </p>
+          </div>
+
+          <div>
+            <h1 className='font-bold text-2xl 2xl:text-center xl:text-center lg:text-center md:text-center sm:text-left mobile:text-left mb-4'>Our Services</h1>
+
+            <p> 
+              <span className='font-bold'>Skills Development:</span> Whether you're an individual seeking to enhance your skillset 
+              or an organization looking to upskill your workforce, we offer a wide range of skill 
+              development programs that cater to diverse industries.
+            </p>
+            <p className='my-4'>
+              <span className='font-bold'>Manpower Training:</span> At Hommaston Limited, we understand the significance of skilled manpower
+              for business growth. Our specialized training for manpower development ensures your employees
+              are competent and productive.
+            </p>
+            <p>
+              <span className='font-bold'>Customized Corporate Training:</span> We provide tailored corporate training solutions to address the unique
+              requirements of your organization. Our programs are designed to align with your
+              goals and enhance overall productivity.
+            </p>
+            <p className='my-4'>
+              <span className='font-bold'>Career Guidance:</span> Our support doesn't end with training. We offer comprehensive career guidance and
+              counseling to help our trainees navigate their professional paths successfully.
+            </p>
+            <p>
+              <span className='font-bold'>Job Placement Assistance:</span> As a dedicated training and development company, we take pride in assisting
+              our trainees in finding suitable employment opportunities in their respective fields.
+            </p>
+          </div>
 
         </div>
 
@@ -17,3 +90,24 @@ const Servicesp = () => {
 }
 
 export default Servicesp
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
