8e1a5909cd9e2cbd7450964ab497c6c8305878f8
patrickashi <patrickpeko1@gmail.com>
2024-02-10 09:45:18 +0100

    new
    add new

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
         <div className='relative w-full h-[300px]'>
             <img src={webdesign2}  alt="img" size={20} className='h-full w-full object-cover z-0' />
             <div className='absolute 2xl:mt-[-55px] mobile:mt-[-20px] mx-auto border-2 flex justify-center 
-            xl:mt-[-55px] lg:mt-[-55px] md:mt-[-55px] sm:mt-[-20px] items-center inset-x-0 bg-gray-700/20
+            xl:mt-[-55px] lg:mt-[-55px] md:mt-[-55px] sm:mt-[-20px] items-center inset-x-0 bg-neutral-50/70
             2xl:mx-20 mobile:mx-5 rounded-[300px] shadow-md shadow-violet-400 z-10'>
-                <h1 className='2xl:text-6xl xl:text-6xl lg:text-6xl mobile:text-3xl py-4 font-bold text-black'>About Us</h1>
+                <h1 className='2xl:text-6xl xl:text-6xl lg:text-6xl mobile:text-3xl py-4 font-bold text-gray-600'>About Us</h1>
             </div>
         </div>
 
