e7d0ff911deb54e75763c8059cda33a2298d70b5
patrickashi <patrickpeko1@gmail.com>
2024-02-10 10:28:06 +0100

    new
    add new

--- src/pages/Contactp.jsx
+++ src/pages/Contactp.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import interiorplaces from "../assets/interiorplaces.jpg";
+import Footer from '../components/Footer';
 
 const Contactp = () => {
   return (
-    <div className='h-screen bg-yellow-400'>
-        <div>
-            <p>Contact</p>
-        </div>
+    <div className='h-screen font-Montserrat'>
+            <div className='relative w-full h-[300px] bg-cover bg-center' style={{ backgroundImage: `url(${interiorplaces})` }}>
+                {/* <img src={hepburnpic}  alt="img" size={20} className='h-full w-full object-cover z-0' /> */}
+                <div className='absolute 2xl:mt-[250px] mobile:mt-[250px] mx-auto border-2 flex justify-center 
+                xl:mt-[250px] lg:mt-[250px] md:mt-[250px] sm:mt-[250px] items-center inset-x-0 bg-gray-700/20
+                2xl:mx-20 mobile:mx-5 rounded-[300px] shadow-md shadow-violet-400'>
+                    <h1 className='2xl:text-5xl xl:text-5xl lg:text-5xl mobile:text-3xl py-4 font-bold 2xl:text-gray-700
+                    mobile:text-gray-600 bg-neutral-50/70 w-full text-center rounded-full'>Get in Touch</h1>
+                </div>
+            </div>
+
+            <div className='my-20 mt-20'>
+              <div className='flex justify-center items-center'>
+                <img src={interiorplaces} alt="img"  className='w-[400px] rounded-xl'/>
+              </div>
+              <p className='text-center my-4'>Contact us </p>
+            </div>
+
+            <Footer />
     </div>
   )
 }
--- src/pages/Ourteamp.jsx
+++ src/pages/Ourteamp.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import domore from "../assets/domore.jpg";
+import Footer from '../components/Footer';
 
 const Ourteamp = () => {
   return (
-    <div className='h-screen bg-green-600'>
-        <div>
-            <p>Our Team</p>
-        </div>
+    <div className='h-screen font-Montserrat'>
+            <div className='relative w-full h-[300px] bg-cover bg-center' style={{ backgroundImage: `url(${domore})` }}>
+                {/* <img src={hepburnpic}  alt="img" size={20} className='h-full w-full object-cover z-0' /> */}
+                <div className='absolute 2xl:mt-[250px] mobile:mt-[250px] mx-auto border-2 flex justify-center 
+                xl:mt-[250px] lg:mt-[250px] md:mt-[250px] sm:mt-[250px] items-center inset-x-0 bg-gray-700/20
+                2xl:mx-20 mobile:mx-5 rounded-[300px] shadow-md shadow-violet-400'>
+                    <h1 className='2xl:text-5xl xl:text-5xl lg:text-5xl mobile:text-3xl py-4 font-bold 2xl:text-gray-700
+                    mobile:text-gray-600 bg-neutral-50/70 w-full text-center rounded-full'>Our Team</h1>
+                </div>
+            </div>
+
+            <div className='2xl:mx-[300px] xl:mx-20 lg:mx-20 md:mx-10 sm:mx-5 mobile:mx-4 pt-20'>
+                <p className='2xl:text-center xl:text-center lg:text-center md:text-center sm:text-left mobile:text-left'>
+                  HOMMASTON team of experienced professionals are responsible for the strategic direction and the day-to-dayoperations
+                  of the organisation.Covering all aspects of operations, sales, product strategy, administration and finance, 
+                  our team works together to ensure the continued success of the organistation
+                </p>
+            </div>
+
+            <div className='flex 2xl:flex-row xl:flex-row lg:flex-row md:flex-row sm:flex-row flex-wrap mobile:flex-col justify-center items-center gap-4 my-8'>
+                <div> <img src={domore} size={20} alt="img" className='w-[400px]' /></div>
+                <div> <img src={domore} alt="img" className='w-[400px]'/></div>
+                <div> <img src={domore} alt="img" className='w-[400px]'/></div>
+            </div>
+
+            <Footer />
     </div>
   )
 }
