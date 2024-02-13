9164feb861278b6f6ade767bc333676f331beb6c
patrickashi <patrickpeko1@gmail.com>
2024-02-13 14:12:15 +0100

    new
    add new

--- src/pages/Hommastonredp.jsx
+++ src/pages/Hommastonredp.jsx
@@ -10,9 +10,9 @@ const Hommastonredp = () => {
     <div className='font-Montserrat'>
          <div className='relative w-full h-[300px] bg-cover bg-center' style={{ backgroundImage: `url(${mechanicalpiping})` }}>
             {/* <img src={hepburnpic}  alt="img" size={20} className='h-full w-full object-cover z-0' /> */}
-            <div className='absolute 2xl:mt-[250px] mobile:mt-[250px] mx-auto border-2 flex justify-center 
-            xl:mt-[250px] lg:mt-[250px] md:mt-[250px] sm:mt-[250px] items-center inset-x-0 bg-neutral-50/70
-            2xl:mx-20 mobile:mx-5 rounded-[300px] shadow-md shadow-violet-400'>
+            <div className='absolute 2xl:mt-[255px] mobile:mt-[265px] mx-auto border-2 flex justify-center 
+            xl:mt-[255px] lg:mt-[255px] md:mt-[265px] sm:mt-[265px] items-center inset-x-0 bg-neutral-50/70
+            2xl:mx-20 mobile:mx-5 rounded-xl shadow-md shadow-violet-400'>
                 <Link to="/Servicesp"> <MdArrowBackIosNew className='mr-3 rounded-md bg-gray-100 w-10 '/> </Link>
                 <h1 className='2xl:text-5xl xl:text-5xl lg:text-5xl sm:text-2xl mobile:text-2xl py-4 font-bold 2xl:text-[#ED1C24]
                 mobile:text-[#ED1C24] text-center rounded-full'>Hommaston Red</h1>
