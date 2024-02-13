090d5e0f9b8b1a1e91a06248dfa8c5f8923453a4
patrickashi <patrickpeko1@gmail.com>
2024-02-13 14:06:13 +0100

    new
    add new

--- src/pages/Classroomredp.jsx
+++ src/pages/Classroomredp.jsx
@@ -10,16 +10,16 @@ const Classroomredp = () => {
 
          <div className='relative w-full h-[300px] bg-cover bg-center' style={{ backgroundImage: `url(${mechanicalpiping})` }}>
             {/* <img src={hepburnpic}  alt="img" size={20} className='h-full w-full object-cover z-0' /> */}
-            <div className='absolute 2xl:mt-[250px] mobile:mt-[250px] mx-auto border-2 flex justify-center 
-            xl:mt-[250px] lg:mt-[250px] md:mt-[250px] sm:mt-[250px] items-center inset-x-0 bg-neutral-50/70
-            2xl:mx-20 mobile:mx-5 rounded-[300px] shadow-md shadow-violet-400'>
+            <div className='absolute py-4 2xl:mt-[240px] mobile:mt-[250px] mx-auto border-2 flex justify-center 
+            xl:mt-[240px] lg:mt-[240px] md:mt-[250px] sm:mt-[250px] items-center inset-x-0 bg-neutral-50/70
+            2xl:mx-20 mobile:mx-5 rounded-xl shadow-md shadow-violet-400'>
 
                 <Link to="/Hommastonredp"> <MdArrowBackIosNew className='mr-3 rounded-md bg-gray-100 w-10 '/> </Link>
                 <h1 className='2xl:text-5xl xl:text-5xl lg:text-5xl mobile:text-2xl py-4 font-bold 2xl:text-[#ED1C24]
                 mobile:text-[#ED1C24] text-center rounded-full'>Classroom Training</h1>
             </div>
 
-            <p className='text-center 2xl:pt-[310px] xl:pt-[310px] lg:pt-[310px] mobile:pt-[294px] font-bold '>Select category to download brochure</p>
+            <p className='text-center 2xl:pt-[323px] xl:pt-[323px] lg:pt-[323px] md:pt-[315px] sm:pt-[315px] mobile:pt-[315px] font-bold'>Select category to download brochure</p>
          </div>
 
          <div className='pt-20 2xl:mx-[200px] xl:mx-20 lg:mx-10 md:mx-6 sm:mx-4 mobile:mx-4 pb-10'>
