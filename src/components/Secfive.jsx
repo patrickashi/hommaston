c3b8be6cb3d6fc411db1b6726e22d4aeda51d423
patrickashi <patrickpeko1@gmail.com>
2024-02-19 15:48:53 +0100

    new
    add new

--- src/components/Secfive.jsx
+++ src/components/Secfive.jsx
@@ -13,25 +13,25 @@ const Secfive = () => {
             <h1 className='text-center text-3xl mb-2'>Our most Popular <span className='text-[#0504AA]'>Courses</span></h1>
             <div className='flex py-16 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col mobile:flex-col mobile:gap-[100px] sm:gap-[100px] md:gap-0 lg:gap-0 xl:gap-0 2xl:gap-5'>
 
-                <div className='px-4 mobile:mx-4 py-4 2xl:w-[380px] mobile:w-[240px] flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
-                    <div className=' 2xl:w-[380px] mobile:w-[200px] flex justify-center'>
-                        <img src={electricalfacilitydesign} className='h-25 w-cover justify-center rounded-md 2xl:w-[300px] mobile:w-[200px]' alt="img" />
+                <div className='px-4 mobile:mx-4 py-4 2xl:w-[380px] mobile:w-[340px] flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
+                    <div className=' 2xl:w-[380px] mobile:w-[300px] flex justify-center'>
+                        <img src={electricalfacilitydesign} className='h-25 w-cover justify-center rounded-md 2xl:w-[300px] mobile:w-[300px]' alt="img" />
                     </div>
-                    <p className='py-2 text-center 2xl:w-[300px] mobile:w-[200px]'>Fundamentals of Electrical Engineering Facility Design</p>
+                    <p className='py-2 text-center 2xl:w-[300px] mobile:w-[300px]'>Fundamentals of Electrical Engineering Facility Design</p>
                 </div>
 
-               <div className='px-4 py-4 mobile:mx-4 2xl:w-[380px] mobile:w-[240px] flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] mobile:justify-center'>
-                    <div className=' 2xl:w-[380px] mobile:w-[200px] flex justify-center '>
-                        <img src={instrumentationandcontrol} className='h-25 w-cover justify-center rounded-md 2xl:w-[300px] mobile:w-[200px]' alt="img" />
+               <div className='px-4 py-4 mobile:mx-4 2xl:w-[380px] mobile:w-[340px] flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] mobile:justify-center'>
+                    <div className=' 2xl:w-[380px] mobile:w-[300px] flex justify-center '>
+                        <img src={instrumentationandcontrol} className='h-25 w-cover justify-center rounded-md 2xl:w-[300px] mobile:w-[300px]' alt="img" />
                     </div>
-                    <p className='py-2 text-center sm:mx-[68px] mobile:mx-[6px] 2xl:mx-0 xl:mx-0 lg:mx-0 mobile:text-sm 2xl:w-[300px] mobile:w-[200px]'>Fundamentals of Instrumentation and control Engineering Facility Design</p>
+                    <p className='py-2 text-center sm:mx-[68px] mobile:mx-[6px] 2xl:mx-0 xl:mx-0 lg:mx-0 mobile:text-sm 2xl:w-[300px] mobile:w-[300px]'>Fundamentals of Instrumentation and control Engineering Facility Design</p>
                 </div>
 
-                <div className='px-4 py-4 mobile:mx-4 2xl:w-[380px] mobile:w-[240px] flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
-                    <div className=' 2xl:w-[380px] mobile:w-[200px] flex justify-center'>
-                        <img src={mechanicalpiping} className='h-25 w-cover justify-center rounded-md 2xl:w-[300px] mobile:w-[200px]' alt="img" />
+                <div className='px-4 py-4 mobile:mx-4 2xl:w-[380px] mobile:w-[340px] flex flex-col items-center shadow-md rounded-md shadow-[#ED1C24] justify-center'>
+                    <div className=' 2xl:w-[380px] mobile:w-[300px] flex justify-center'>
+                        <img src={mechanicalpiping} className='h-25 w-cover justify-center rounded-md 2xl:w-[300px] mobile:w-[300px]' alt="img" />
                     </div>
-                    <p className='py-2 sm:mx-[68px] mobile:mx-[6px] text-center 2xl:mx-0 xl:mx-0 lg:mx-0 2xl:w-[300px] mobile:w-[200px]'>Fundamentals of Mechanical/Piping Engineering Facility Design</p>
+                    <p className='py-2 sm:mx-[68px] mobile:mx-[6px] text-center 2xl:mx-0 xl:mx-0 lg:mx-0 2xl:w-[300px] mobile:w-[300px]'>Fundamentals of Mechanical/Piping Engineering Facility Design</p>
                 </div>
             </div>
 
