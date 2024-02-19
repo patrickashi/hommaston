40a303e16a73baff49b9c8cdf872eed458d0e00a
patrickashi <patrickpeko1@gmail.com>
2024-02-19 15:52:36 +0100

    new
    add new

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -51,7 +51,7 @@ const Faq = () => {
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
             xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
                 <div className='flex justify-between'>
-                    <div className='flex '>Do You Offer E-Learning?</div>
+                    <div className='flex font-bold'>Do You Offer E-Learning?</div>
                     <button onClick={handleClick} className={ hide ?  '' : ""}>
                         { hide ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
@@ -66,7 +66,7 @@ const Faq = () => {
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
             xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
                 <div className='flex justify-between'>
-                    <div className='flex '>Can I Pay For Course Online Using My Credit Card?</div>
+                    <div className='flex font-bold'>Can I Pay For Course Online Using My Credit Card?</div>
                     <button onClick={handleClickSecond}>
                         { hideSecond ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
@@ -81,7 +81,7 @@ const Faq = () => {
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
             xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
                 <div className='flex justify-between'>
-                    <div className='flex '>Is Hommaston Limited On Social Media?</div>
+                    <div className='flex font-bold'>Is Hommaston Limited On Social Media?</div>
                     <button onClick={handleClickThird} className={ hide ?  '' : ""}>
                         { hideThird ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
@@ -96,7 +96,7 @@ const Faq = () => {
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
             xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
                 <div className='flex justify-between'>
-                    <div className='flex '>What Payment Method Or Form Do You Accept?</div>
+                    <div className='flex font-bold'>What Payment Method Or Form Do You Accept?</div>
                     <button onClick={handleClickFourth} className={ hide ?  '' : ""}>
                         { hideFourth ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
@@ -111,7 +111,7 @@ const Faq = () => {
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
             xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
                 <div className='flex justify-between'>
-                    <div className='flex '>Are All Hommaston Courses Certified?</div>
+                    <div className='flex font-bold'>Are All Hommaston Courses Certified?</div>
                     <button onClick={handleClickFifth} className={ hide ?  '' : ""}>
                         { hideFifth ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
@@ -126,7 +126,7 @@ const Faq = () => {
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
             xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
                 <div className='flex justify-between'>
-                    <div className='flex '>Where Does the Training Take Place?</div>
+                    <div className='flex font-bold'>Where Does the Training Take Place?</div>
                     <button onClick={handleClickSixth} className={ hide ?  '' : ""}>
                         { hideSixth ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
@@ -139,9 +139,9 @@ const Faq = () => {
             </div>
 
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
-            xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
+            xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4 font-bold'>
                 <div className='flex justify-between'>
-                    <div className='flex '>What Is the Mode Of Training?</div>
+                    <div className='flex font-bold'>What Is the Mode Of Training?</div>
                     <button onClick={handleClickSeventh} className={ hide ?  '' : ""}>
                         { hideSeventh ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
@@ -156,7 +156,7 @@ const Faq = () => {
             <div className='py-4 px-4 bg-white text-[#170F49] 2xl:mx-40 mobile:mx-4 my-4 rounded-2xl
             xl:mx-40 lg:mx-20 md:mx-20 sm:mx-4'>
                 <div className='flex justify-between'>
-                    <div className='flex '>How Do You Measure Attendee Satisfaction?</div>
+                    <div className='flex font-bold'>How Do You Measure Attendee Satisfaction?</div>
                     <button onClick={handleClickEight} className={ hide ?  '' : ""}>
                         { hideEight ? <FiPlus className='text-[#6F6C90] text-lg bg-gray-300 rounded-lg' size={40}/> : <LuMinus className='bg-[#4A3AFF] rounded-lg text-white' size={40}/>}
                     </button>
