4e3940782e23726079fd0950f9a0688d6755d930
patrickashi <patrickpeko1@gmail.com>
2024-03-04 08:57:49 +0100

    new
    add new

---
+++ src/components/Myform.jsx
@@ -0,0 +1,127 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import contactmapbg from "../assets/contactmapbg.png";
+
+import { FaCirclePlay } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { LiaLinkedinIn } from "react-icons/lia";
+
+
+const Myform = () => {
+    const [formData, setFormData] = useState({
+      firstname: '',
+      lastname: '',
+      email: '',
+      address: '',
+      phone_number: '',
+      message: ''
+    });
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData(prevState => ({
+        ...prevState,
+        [name]: value
+      }));
+    };
+  
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/form_submission/', formData);
+        console.log('Form submitted successfully:', response.data);
+        // Reset form after successful submission
+        setFormData({
+          firstname: '',
+          lastname: '',
+          email: '',
+          address: '',
+          phone_number: '',
+          message: ''
+        });
+      } catch (error) {
+        console.error('Error submitting form:', error);
+      }
+    };
+
+
+    return (
+        // <form onSubmit={handleSubmit}>
+        //     <input type="text" name="name" value={formData.name} onChange={handleChange} />
+        //     <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        //     <textarea name="message" value={formData.message} onChange={handleChange} />
+        //     <button type="submit">Submit</button>
+        // </form>
+
+       
+
+        <div className='pt-20 mb-40 h-[400px] w-full bg-cover bg-start py-8 flex justify-evenly'
+        style={{ backgroundImage:  `url(${contactmapbg})` }}>
+
+            <div className='mobile:hidden 2xl:block xl:block lg:block md:block'>
+            <p className='text-2xl font-bold text-[#FF0000] animate-ping'>Contact Us</p>
+            </div>
+
+            <div className='2xl:w-[450px] xl:w-[450px] lg:w-[450px] md:w-[450px] mobile:w-[350px] 2xl:h-[450px] xl:h-[450px] lg:h-[450px] md:h-[450px] mobile:h-[500px]  pt-6 pb-12
+            bg-white rounded-2xl 2xl:mt-[-90px] xl:mt-[-90px] lg:mt-[-90px] md:mt-[-90px] mobile:mt-[-100px] border-2
+            border-gray-300 shadow-lg shadow-[#FF0000]'>
+
+                <form onSubmit={handleSubmit}>
+                    <div className='flex mobile:flex-col 2xl:flex-row xl:flex-row lg:flex-row md:flex-row gap-2 px-4'>
+                        <div className=''>
+                            <input type="text" name="firstname" value={formData.firstname} onChange={handleChange} className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='firstname' />
+                        </div>
+                        <div>
+                            <input type="text" name="lastname" value={formData.lastname} onChange={handleChange} className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='lastname' />
+                        </div>
+                    </div>
+
+                    <div className='px-4'>
+                        <input type="email" name="email" value={formData.email} onChange={handleChange} className='rounded-lg bg-gray-300 pl-2 my-2 w-full py-4 ' placeholder='Email' />
+                    </div>
+
+                    <div className='flex mobile:flex-col 2xl:flex-row xl:flex-row lg:flex-row md:flex-row gap-2 px-4'>
+                        <div className=''>
+                            <input type="text" name="address" value={formData.address} onChange={handleChange} className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='Address' />
+                        </div>
+                        <div>
+                            <input type="tel" name="phone_number" value={formData.phone_number} onChange={handleChange} className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='phone no' />
+                        </div>
+                    </div>
+
+                    <div className='px-4'>
+                        <textarea name="message" value={formData.message} onChange={handleChange} className='rounded-lg bg-gray-300 pl-2 my-2 w-full 2xl:pb-20 xl:pb-20 lg:pb-20 md:pb-20 mobile:pb-10 py-4 text-sm' placeholder='other information you want us to know' />
+                    </div>
+
+                    <div className='flex 2xl:mt-8 xl:mt-8 lg:mt-8 md:mt-8 mobile:mt-2 justify-between'>
+
+                        <div className='flex items-center ml-2'>
+                        <ul className='flex gap-8'>
+                            <li><LiaLinkedinIn size={20} className='shadow-md shadow-gray-300 rounded-full'/></li>
+                            <li> <FaInstagramSquare size={20} className='shadow-md shadow-gray-300 rounded-full' /> </li>
+                            <li> <FaCirclePlay size={20} className='shadow-md shadow-gray-300 rounded-full'/></li>
+                            <li><FaFacebook size={20} className='shadow-md shadow-gray-300 rounded-full'/></li>
+                        </ul>
+                        </div> 
+
+                        <div>
+                        <button type="submit" className='bg-[#0504AA] text-white rounded-tl rounded-bl px-4 py-2 hover:scale-105 duration-200'>Send Message</button>
+                        </div>
+                        
+                    </div>
+                    
+                </form>
+            </div> 
+
+
+
+            
+        </div>
+
+
+        
+    );
+};
+
+export default Myform;
--- src/pages/Contactp.jsx
+++ src/pages/Contactp.jsx
@@ -1,12 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Footer from '../components/Footer';
 import contactmapbg from "../assets/contactmapbg.png";
-import { FaCirclePlay } from "react-icons/fa6";
-import { FaInstagramSquare } from "react-icons/fa";
 
-import { FaFacebook } from "react-icons/fa";
-import { LiaLinkedinIn } from "react-icons/lia";
-import Contactform from '../components/Contactform';
+import Myform from '../components/Myform';
 
 const Contactp = () => {
   return (
@@ -25,72 +21,11 @@ const Contactp = () => {
                 <h1 className='2xl:hidden xl:hidden lg:hidden md:hidden mobile:block animate-bounce'>Contact us</h1>
             </div>
 
-            {/* <Contactform /> */}
+            <Myform />
 
             
 
-            <div className='pt-20 mb-40 h-[400px] w-full bg-cover bg-start py-8 flex justify-evenly'
-            style={{ backgroundImage:  `url(${contactmapbg})` }}>
-
-                <div className='mobile:hidden 2xl:block xl:block lg:block md:block'>
-                  <p className='text-2xl font-bold text-[#FF0000] animate-ping'>Contact Us</p>
-                </div>
-
-                <div className='2xl:w-[450px] xl:w-[450px] lg:w-[450px] md:w-[450px] mobile:w-[350px] 2xl:h-[450px] xl:h-[450px] lg:h-[450px] md:h-[450px] mobile:h-[500px]  pt-6 pb-12
-                bg-white rounded-2xl 2xl:mt-[-90px] xl:mt-[-90px] lg:mt-[-90px] md:mt-[-90px] mobile:mt-[-100px] border-2
-                  border-gray-300 shadow-lg shadow-[#FF0000]'>
-
-                    <form >
-                        <div className='flex mobile:flex-col 2xl:flex-row xl:flex-row lg:flex-row md:flex-row gap-2 px-4'>
-                            <div className=''>
-                                <input type="text" className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='firstname' />
-                            </div>
-                            <div>
-                                <input type="text" className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='lastname' />
-                            </div>
-                        </div>
-
-                        <div className='px-4'>
-                            <input type="text" className='rounded-lg bg-gray-300 pl-2 my-2 w-full py-4 ' placeholder='Email' />
-                        </div>
-
-                        <div className='flex mobile:flex-col 2xl:flex-row xl:flex-row lg:flex-row md:flex-row gap-2 px-4'>
-                            <div className=''>
-                                <input type="text" className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='Address' />
-                            </div>
-                            <div>
-                                <input type="text" className='rounded-lg bg-gray-300 pl-2 mobile:w-full py-4' placeholder='phone no' />
-                            </div>
-                        </div>
-
-                        <div className='px-4'>
-                            <input type="text" className='rounded-lg bg-gray-300 pl-2 my-2 w-full 2xl:pb-20 xl:pb-20 lg:pb-20 md:pb-20 mobile:pb-10 py-4 text-sm' placeholder='other information you want us to know' />
-                        </div>
-
-                        <div className='flex 2xl:mt-8 xl:mt-8 lg:mt-8 md:mt-8 mobile:mt-2 justify-between'>
-
-                            <div className='flex items-center ml-2'>
-                              <ul className='flex gap-8'>
-                                <li><LiaLinkedinIn size={20} className='shadow-md shadow-gray-300 rounded-full'/></li>
-                                <li> <FaInstagramSquare size={20} className='shadow-md shadow-gray-300 rounded-full' /> </li>
-                                <li> <FaCirclePlay size={20} className='shadow-md shadow-gray-300 rounded-full'/></li>
-                                <li><FaFacebook size={20} className='shadow-md shadow-gray-300 rounded-full'/></li>
-                             </ul>
-                            </div> 
-
-                            <div>
-                              <button className='bg-[#0504AA] text-white rounded-tl rounded-bl px-4 py-2 hover:scale-105 duration-200'>Send Message</button>
-                            </div>
-                            
-                        </div>
-                        
-                    </form>
-                </div> 
-
            
-
-                
-            </div>
 
             <Footer />
     </div>
