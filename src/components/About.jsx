6f65935d2fd2dfa443f3d08a402e5d7c77e7dfbb
patrickashi <patrickpeko1@gmail.com>
2024-03-20 22:24:43 +0100

    new
    add new

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,12 +20,31 @@ const About = () => {
 
         <div className='py-16 mt-10 2xl:mx-40 xl:mx-20 lg:mx-6 md:mx-6 sm:mx-4 mobile:mx-4'>
 
-          <p className='2xl:text-center xl:text-center lg:text-center md:text-center sm:text-center mobile:text-center 2xl:text-lg xl:text-lg lg:text-lg md:text-lg mobile:text-md
+          {/* <p className='2xl:text-center xl:text-center lg:text-center md:text-center sm:text-center mobile:text-center 2xl:text-lg xl:text-lg lg:text-lg md:text-lg mobile:text-md
           2xl:mx-20 xl:mx-20 lg:mx-6 md:mx-2 mobile:mx-4'>
-          Hommaston operates two core divisions. Red Division (Hommaston Engineering) focal delivery of choice skillsets for design engineering, facility maintenance and operations 
-          supports competency and performance.  Blue division (Hommaston Digital) handles human capital development, drone tech, cloud computing, corporate soft skills and all other 
-          non-engineering, certification preparatory courses.
-          </p>
+          
+          </p> */}
+
+          <div className=' grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 2xl:mx-20 xl:mx-20 lg:mx-6 md:mx-6 mobile:mx-4'>
+          
+            <div className=''>
+              <p className='flex 2xl:text-start xl:text-start lg:text-start md:text-start mobile:text-start mt-2'>
+                Hommaston operates two core divisions. Red Division (Hommaston Engineering) focal delivery of choice skillsets for design engineering, facility maintenance and operations 
+                supports competency and performance.  Blue division (Hommaston Digital) handles human capital development, drone tech, cloud computing, corporate soft skills and all other 
+                non-engineering, certification preparatory courses.
+              </p>
+
+              <p className='flex 2xl:text-start xl:text-start lg:text-start md:text-start mobile:text-start mt-8'>
+                At Our Training Facilities, we prioritize creating a conducive learning environment that promotes comfort and enhances 
+                the learning experience. Our courses are carefully crafted to ensure that our learners acquire, retain, and apply 
+                knowledge effectively.
+              </p>
+            </div>
+
+            <div className='flex justify-end md:w-full  '>
+              <img src={hommastonashlogo} size={20} className='w-80 md:w-full lg:w-full' alt="img" />
+            </div>
+        </div>
 
           <div className='my-8 2xl:mx-20 xl:mx-20 lg:mx-6 md:mx-2 mobile:mx-4'>
             <h1 className='text-2xl text-center font-bold'>Training Gallery</h1>
@@ -41,42 +60,40 @@ const About = () => {
           </div>
         </div>
 
-        <div className=' grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 2xl:mx-40 xl:mx-20 lg:mx-6 md:mx-6 mobile:mx-4'>
-          <div className='flex justify-center md:w-full '>
-            <img src={hommastonashlogo} size={20} className='w-80 md:w-full lg:w-full' alt="img" />
-          </div>
-          <div className=''>
-            <p className='flex 2xl:text-end xl:text-end lg:text-end md:text-end mobile:text-start mt-2'>
-              At Our Training Facilities, we prioritize creating a conducive learning environment that promotes comfort and enhances the 
-              learning experience. Our courses are carefully crafted to ensure that our learners acquire,
-              retain, and apply knowledge effectively.
-            </p>
+        <div className='2xl:mx-60  xl:mx-40  lg:mx-14  md:mx-6 mobile:mx-4 my-14 mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2
+        border-b pb-4'>
 
-            <p className='flex 2xl:text-end xl:text-end lg:text-end md:text-end mobile:text-start mt-8'>
-              We are a world-class training delivery organization supported by international collaborative 
-              synergies for specialized deliveries. We offer courses and certifications through
-              various methods such as Classroom training, Corporate bespoke, Online learning,
-              Virtual gaming, Blended learning (where applicable), and In-house scheduled approaches.
-              We ensure that our training methods are tailored to meet the convenience of our clients.
+          <div className='flex 2xl:justify-center lg:justify-center xl:justify-center md:justify-center mobile:justify-start'>
+              <img src={vission} size={20} className='' alt="img" />
+          </div>
+
+          <div className=' items-center'>
+            <h1 className='2xl:text-end md:text-end lg:text-end xl:text-end mobile:text-start font-bold text-2xl mb-4'>More About Us</h1>
+            <p className='flex 2xl:text-end xl:text-end lg:text-end md:text-end mobile:text-start'>
+              We are a world-class training delivery organization supported by international collaborative synergies for
+              specialized deliveries. We offer courses and certifications through various methods such as Classroom training,
+              Corporate bespoke, Online learning, Virtual gaming, Blended learning (where applicable), and In-house scheduled
+              approaches. We ensure that our training methods are tailored to meet the convenience of our clients.
+            </p>
+            <p className='flex 2xl:text-end xl:text-end lg:text-end md:text-end mobile:text-start mt-6'>
+              Our company offers workforce skills enhancement programs that are designed to cater to the learning 
+              needs of auditory, visual, and kinaesthetic learners on a global scale. We provide outcome measurements
+              and competency evaluation for all our training programs. Hommaston’s promise is “we empower you to perform”
             </p>
           </div>
+          
         </div>
 
-        <div className='2xl:mx-40 xl:mx-20 lg:mx-6 md:mx-6 mobile:mx-4 border-t border-b pb-6 mt-4'>
-          <p className='flex 2xl:text-center xl:text-center lg:text-center md:text-center mobile:text-start mt-8'>
-          Our company offers workforce skills enhancement programs that are designed to cater to the learning needs
-           of auditory, visual, and kinaesthetic learners on a global scale. We provide outcome measurements and
-            competency evaluation for all our training programs. Hommaston’s promise is “we empower you to perform”
-          </p>
-        </div>
+        
 
-        <div className='2xl:mx-40  xl:mx-20  lg:mx-6  md:mx-6 mobile:mx-4 my-14 mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2
+        <div className='2xl:mx-60  xl:mx-40  lg:mx-14  md:mx-6 mobile:mx-4 my-14 mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2
         border-b pb-6'>
           <div className='flex 2xl:justify-center xl:justify-center lg:justify-center md:justify-center mobile:justify-end mobile:block 2xl:hidden xl:hidden lg:hidden md:hidden'>
               <img src={mission} size={20} className='w-60' alt="img" />
           </div>
 
-          <div className='flex items-center'>
+          <div className=''>
+            <h1 className='font-bold text-2xl mb-4'>Our Mission</h1>
             <p>
               To provide a reputable skills enhancement platform that proficiently
               transforms unskilled resource personnel into invaluable workforce assets.
@@ -87,14 +104,15 @@ const About = () => {
           </div>
         </div>
 
-        <div className='2xl:mx-40  xl:mx-20  lg:mx-6  md:mx-6 mobile:mx-4 my-14 mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2
+        <div className='2xl:mx-60  xl:mx-40  lg:mx-14  md:mx-6 mobile:mx-4 my-14 mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2
         border-b pb-4'>
 
           <div className='flex 2xl:justify-center lg:justify-center xl:justify-center md:justify-center mobile:justify-start'>
               <img src={vission} size={20} className='w-60' alt="img" />
           </div>
 
-          <div className='flex items-center'>
+          <div className=''>
+            <h1 className='font-bold text-2xl mb-4 2xl:text-end xl:text-end lg:text-end md:text-end sm:text-start'>Our Vission</h1>
             <p className='flex 2xl:text-end xl:text-end lg:text-end md:text-end mobile:text-start'>
               To be the prominent standard in global delivery of qualitative capacity building services.
             </p>
@@ -102,13 +120,14 @@ const About = () => {
           
         </div>
 
-        <div className='flex 2xl:mx-40  xl:mx-20  lg:mx-6  md:mx-6 mobile:mx-4 my-14 mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2
+        <div className='2xl:mx-60  xl:mx-40  lg:mx-14  md:mx-6 mobile:mx-4 my-14 mobile:grid 2xl:grid-cols-2 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2
         border-b pb-6'>
           <div className='flex justify-center items-center mobile:block 2xl:hidden xl:hidden lg:hidden md:hidden'>
               <img src={corevalues} size={20} className='w-80 object-contain' alt="img" />
           </div>
 
           <div>
+            <h1 className='font-bold text-2xl mb-4'>Our Core Values</h1>
             <p>
             Our core value ‘‘WI3N’’ is the embodiment of our organizational culture, definitive of our corporate character and
              service delivery expectations. It is the bedrock reference for interface considerations, our conduct with stakeholders
