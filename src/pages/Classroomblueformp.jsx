ca9500f5e0cdd8fa89d87f16a4ce00c399a0b89d
patrickashi <patrickpeko1@gmail.com>
2024-03-25 15:27:48 +0100

    new
    add new

--- src/pages/Classroomblueformp.jsx
+++ src/pages/Classroomblueformp.jsx
@@ -16,7 +16,7 @@ const Classroomblueformp = () => {
             2xl:mx-20 mobile:mx-5 rounded-xl shadow-md shadow-violet-400'>
                 <Link to="/Classroomblue"> <MdArrowBackIosNew className='mr-3 rounded-md bg-gray-100 w-10 '/> </Link>
                 <h1 className='2xl:text-5xl xl:text-5xl lg:text-5xl sm:text-2xl mobile:text-2xl py-4 font-bold 2xl:text-[#0504AA]
-                mobile:text-[#0504AA]  text-center rounded-full'>Hommaston Blue Form</h1>
+                mobile:text-[#0504AA]  text-center rounded-full'>Hommaston Blue</h1>
             </div>
         </div>
 
